eefe7acc0256b4cd3ae59807368b3dcd5104d5ff
shatter-star <asiddharth_csm206601@mgit.ac.in>
2024-04-07 15:12:16 +0530

    Remove MLflow code for downloading model artifacts

--- hey.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-
-# Define the run_id of the MLflow run containing the model artifacts
-run_id = "d3b22973d11e4765a60d82a68edca4d7"
-
-# Download model artifacts locally
-local_model_path = "local_model"
-mlflow.artifacts.download_artifacts(run_id=run_id, dst_path=local_model_path)
